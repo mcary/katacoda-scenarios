930d7bc4831300aaed6a1fab198db0e996fc8bff
Marcel M. Cary <marcel@oreilly.com>
2021-09-13 09:28:38 -0700

    Install Ansible AWX using the docker-compose method

--- awx/foreground.sh
+++ awx/foreground.sh
@@ -3,4 +3,8 @@
 # This script is run as if pasted into the terminal, so no "-x" is needed
 # to show the commands that were run.  Comments will be stripped.
 
+while ! [ -f install-awx.sh ]; do echo -n .; sleep 1; done; echo
 ./install-awx.sh
+
+# Create admin user for AWX
+$ docker exec -ti tools_awx_1 awx-manage createsuperuser
