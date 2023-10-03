23b8b650bd5aec746c5f66e8a8efee333c61d5ae
Marcel M. Cary <marcel@oreilly.com>
2023-10-03 15:16:35 -0700

    Move rails-short-sandbox steps 1-2 to background script

--- rails-short-sandbox/background.sh
+++ rails-short-sandbox/background.sh
@@ -1,22 +1,23 @@
 # Fail fast on errors.
 set -e
 
-exit
-
 # Redirect output of the current script.
 exec > /opt/background.log 2>&1
 
-# Say what's happening for diagnostic purposes.
+# Say what's happening for diagnostic purposes, shown by foreground.sh.
+PS4='+ Running: '
 set -x
 
-# Wait for vscode to start, else calling code-server will start a new
-# server rather than opening a file in the existing server.
-while ! [ -d /opt/.katacodacode/user-data/User/workspaceStorage/ ]; do
-  date -Ins
-  sleep 0.5
-done
+# This ruby image doesn't run everything in a shell that has RVM configured.
+# So load it as if we were starting from a login shell:
+. /etc/profile
 
-sleep 0.1 # Wait a bit more for safety.
+# Assume Rails 3.2.2 is installed.
 
-# Open a file in the existing server.
-/opt/vscode/bin/code-server example-app/src/App.js
+# The `--no-document` skips a slow documentation build process.
+gem install rails --no-document  # ~10 sec
+
+apt-get update -q
+DEBIAN_FRONTEND=noninteractive apt-get install -yq tzdata  # ~6 sec
+
+echo "Rails is installed!"
